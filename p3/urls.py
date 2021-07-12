df683be3eff64dee4277c4390e82b636089c27eb
Brandon Lee <bblee@berkeley.edu>
2021-07-11 18:13:54 -0700

    fixed bugs in views.py and created layout in coloringPage

--- p3/urls.py
+++ p3/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('coloring/', include('coloring.urls')),
 ]
+def coloringPage(request):
+    return render(request, 'coloring/templates/coloring/coloringPage.html')
